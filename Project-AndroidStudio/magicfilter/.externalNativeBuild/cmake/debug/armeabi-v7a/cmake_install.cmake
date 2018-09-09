f1d52519deee62a9c1116d2acb4eff1ff2c2e285
jerryzcxisme <jerryzcxisme@gmail.com>
2018-09-09 15:32:36 +0800

    gitignore``````

--- Project-AndroidStudio/magicfilter/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
+++ Project-AndroidStudio/magicfilter/.externalNativeBuild/cmake/debug/armeabi-v7a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zjerry/Downloads/MagicCamera/Project-AndroidStudio/magicfilter
+# Install script for directory: /Users/zjerry/Documents/MagicCamera/Project-AndroidStudio/magicfilter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zjerry/Downloads/MagicCamera/Project-AndroidStudio/magicfilter/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zjerry/Documents/MagicCamera/Project-AndroidStudio/magicfilter/.externalNativeBuild/cmake/debug/armeabi-v7a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
